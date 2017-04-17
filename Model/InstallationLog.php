8049df9f0285446e35bb3bc031f7c94e8b051b9b
miky <mathias89100@gmail.com>
2017-04-17 03:14:27 +0200

    Merge branch 'master' of github.com:mikyprog/AdminBundle
    # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est
    # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet.
    #
    # Les lignes commençant par '#' seront ignorées, et un message vide
    # abandonne la validation.

---
+++ Model/InstallationLog.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 27/03/17
+ * Time: 22:41
+ */
+
+namespace Miky\Component\Installer\Model;
+
+
+class InstallationLog
+{
+
+}
---
+++ Model/UpdateLog.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 27/03/17
+ * Time: 22:42
+ */
+
+namespace Miky\Component\Installer\Model;
+
+
+class UpdateLog
+{
+
+}
